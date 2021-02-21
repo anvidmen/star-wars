a8c04467c56104ef3f5e1edce1721fc317511428
Juan David Om <anvidmen@gmail.com>
2021-02-21 21:15:44 +0100

    add axios implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import starWars from 'api/starWars'
+import Home from 'screens/Home/Home'
+import Login from 'screens/UserFlow/Login/Login'
+import Register from 'screens/UserFlow/Register/Register'
+import Starships from 'screens/UserFlow/Starships/Starships'
 
-function App () {
+const App = () => {
+  const [starShips, setStarShips] = useState([])
+
+  useEffect(() => {
+    const fetchStarShips = async (searchShips) => {
+      try {
+        const { data: { results } } = await starWars.get('starships', {
+          params: {
+            results: searchShips
+          }
+        })
+        setStarShips(results)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchStarShips()
+  }, [])
+
+  console.log('data', starShips)
   return (
-    <div>
-      <header>
-        <h1>Star Wars</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/starships'>
+          <Starships data={starShips} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
 }
 
